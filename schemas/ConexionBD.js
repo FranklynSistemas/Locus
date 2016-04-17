9ed03a32975dab775817d3588bad24c96f256af1
FranklynSistemas <Franking.sistemas@gmail.com>
2016-04-17 17:27:29 -0500

    produccion

--- schemas/ConexionBD.js
+++ schemas/ConexionBD.js
@@ -1,4 +1,4 @@
 var mongoose = require('mongoose');
 //GoogleCloud
-//mongoose.connect('mongodb://104.154.47.36:80/pageudec');
-mongoose.connect('mongodb://localhost/pageudec');
+mongoose.connect('mongodb://104.154.47.36:80/pageudec');
+//mongoose.connect('mongodb://localhost/pageudec');
